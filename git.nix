8600e1b59bf3725462fb6669b810a0815acbf875
Alex van de Sandt <me@alexvds.com>
2026-04-25 13:14:24 -0500

    Add jujutsu config

--- git.nix
+++ git.nix
@@ -19,4 +19,24 @@
       commit.gpgsign = true;
     };
   };
+
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      user = {
+        email = "me@alexvds.com";
+        name = "Alex van de Sandt";
+      };
+
+      signing = {
+        sign-all = true;
+        backend = "ssh";
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHJ9zhEHFJIpZ4qr6iu3Cqca3mquGCGyiIrcI0e3jXLZ";
+        backends.ssh = {
+          program = "${pkgs._1password-gui}/bin/op-ssh-sign";
+          allowedSignersFile = "~/.config/git/allowedSigners";
+        };
+      };
+    };
+  };
 }
