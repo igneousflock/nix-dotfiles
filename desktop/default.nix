3538327f0419e60a41969cff293268587b52163a
Alex van de Sandt <me@alexvds.com>
2026-05-05 12:30:11 -0500

    Port hyprland conf to nix

--- desktop/default.nix
+++ desktop/default.nix
@@ -2,16 +2,9 @@
 {
   imports = [
     ./cursor.nix
+    ./hyprland.nix
     ./hyprpaper.nix
   ];
-
-  # Native hyprland config
-  home.file = {
-    hyprland = {
-      target = ".config/hypr/hyprland.conf";
-      source = ./hyprland.conf;
-    };
-  };
 
   programs.waybar.enable = true;
 
