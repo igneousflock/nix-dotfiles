b0b14fab13f9fb8e56ca1c5d8bc21a7206e9eb15
Alex van de Sandt <me@alexvds.com>
2026-07-01 18:29:18 -0500

    Harden sshd further

--- modules/sshd.nix
+++ modules/sshd.nix
@@ -4,13 +4,20 @@
       services.openssh = {
         enable = true;
         openFirewall = true;
+        ports = [ 6767 ];
         settings = {
           PasswordAuthentication = false;
           KbdInteractiveAuthentication = false;
           PermitRootLogin = "no";
           AllowUsers = [ "catbird" ];
+          LoginGraceTime = 30;
           MaxAuthTries = 3;
+          AllowTcpForwarding = "no";
+          ClientAliveInterval = 300;
+          ClientAliveCountMax = 2;
           PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
+          PermitEmptyPasswords = "no";
+          X11Forwarding = false;
         };
       };
     };
