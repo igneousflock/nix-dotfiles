298985d844630eb2b35e46318ec7b83d4bac79a8
Alex van de Sandt <me@alexvds.com>
2026-07-01 22:57:21 -0500

    Simplify with let/in binding

--- modules/atproto/tranquil.nix
+++ modules/atproto/tranquil.nix
@@ -9,73 +9,78 @@ in
   };
 
   den.aspects.tranquil = { user, ... }: {
-    nixos = { config, pkgs, ... }: {
-      imports = [ inputs.tranquil-pds.nixosModules.default ];
+    nixos =
+      { config, pkgs, ... }:
+      let
+        secrets = config.sops.secrets;
+      in
+      {
+        imports = [ inputs.tranquil-pds.nixosModules.default ];
 
-      services.tranquil-pds = {
-        enable = true;
+        services.tranquil-pds = {
+          enable = true;
 
-        database.createLocally = true;
+          database.createLocally = true;
 
-        settings = {
-          server = {
-            inherit hostname;
-            age_assurance_override = true;
-            contact_email = email;
-            max_blob_size = 1024 * 1014 * 1; # 1 GiB
+          settings = {
+            server = {
+              inherit hostname;
+              age_assurance_override = true;
+              contact_email = email;
+              max_blob_size = 1024 * 1014 * 1; # 1 GiB
+            };
+
+            email = {
+              from_address = email;
+
+              smarthost = {
+                host = "smtp.fastmail.com";
+                port = 587;
+              };
+            };
           };
 
-          email = {
-            from_address = email;
+          environmentFiles = [ secrets.tranquil-pds.path ];
+        };
 
-            smarthost = {
-              host = "smtp.fastmail.com";
-              port = 587;
+        networking.firewall = {
+          enable = true;
+          allowedTCPPorts = [
+            80
+            443
+          ];
+        };
+
+        services.caddy = {
+          enable = true;
+          package = pkgs.caddy.withPlugins {
+            plugins = [ "github.com/caddy-dns/route53@v1.6.2" ];
+            hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
+          };
+
+          virtualHosts = {
+            ${hostname} = {
+              # by default, tranquil runs on port 3000.
+              # You can change this with the tranquil-pds.settings.server.port option in the service config.
+              extraConfig = ''
+                reverse_proxy localhost:3000
+              '';
             };
           };
         };
 
-        environmentFiles = [ config.sops.secrets.tranquil-pds.path ];
-      };
+        sops = {
+          defaultSopsFile = ../../secrets/pds.env;
+          secrets = {
+            tranquil-pds = {
+              format = "dotenv";
+              key = "";
 
-      networking.firewall = {
-        enable = true;
-        allowedTCPPorts = [
-          80
-          443
-        ];
-      };
-
-      services.caddy = {
-        enable = true;
-        package = pkgs.caddy.withPlugins {
-          plugins = [ "github.com/caddy-dns/route53@v1.6.2" ];
-          hash = "sha256-F/jqR4iEsklJFycTjSaW8B/V3iTGqqGOzwYBUXxRKrc=";
-        };
-
-        virtualHosts = {
-          ${hostname} = {
-            # by default, tranquil runs on port 3000.
-            # You can change this with the tranquil-pds.settings.server.port option in the service config.
-            extraConfig = ''
-              reverse_proxy localhost:3000
-            '';
+              owner = user.name;
+              group = "wheel";
+            };
           };
         };
       };
-
-      sops = {
-        defaultSopsFile = ../../secrets/pds.env;
-        secrets = {
-          tranquil-pds = {
-            format = "dotenv";
-            key = "";
-
-            owner = user.name;
-            group = "wheel";
-          };
-        };
-      };
-    };
   };
 }
