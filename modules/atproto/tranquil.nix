dfdfcb073b738b038043a4eaf05195cd3a46ca02
Alex van de Sandt <me@alexvds.com>
2026-07-01 19:52:00 -0500

    Enable reverse proxy and expose PDS

--- modules/atproto/tranquil.nix
+++ modules/atproto/tranquil.nix
@@ -14,12 +14,34 @@
           server = {
             hostname = "pds.alexvds.com";
             age_assurance_override = true;
-            contact_email = "me@alexvds.com";
+            contact_email = "pds@alexvds.com";
             max_blob_size = 1024 * 1014 * 1; # 1 GiB
           };
         };
 
         environmentFiles = [ config.sops.secrets.tranquil-pds.path ];
+      };
+
+      networking.firewall = {
+        enable = true;
+        allowedTCPPorts = [
+          80
+          443
+        ];
+      };
+
+      services.caddy = {
+        enable = true;
+
+        virtualHosts = {
+          "pds.alexvds.com" = {
+            # by default, tranquil runs on port 3000.
+            # You can change this with the tranquil-pds.settings.server.port option in the service config.
+            extraConfig = ''
+              reverse_proxy localhost:3000
+            '';
+          };
+        };
       };
 
       sops = {
