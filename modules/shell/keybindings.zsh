e368f733b0cb9fbec38341873869f1b78e739c51
Alex van de Sandt <me@alexvds.com>
2026-08-07 13:32:48 -0500

    Fix the delete key in zsh

--- modules/shell/keybindings.zsh
+++ modules/shell/keybindings.zsh
@@ -1,3 +1,8 @@
 bindkey '\e[B' history-search-forward
 bindkey '\e[A' history-search-backward
 bindkey '^R' history-incremental-pattern-search-backward
+
+typeset -g -A key
+key[Delete]="${terminfo[kdch1]}"
+
+[[ -n "${key[Delete]}" ]] && bindkey -- "${key[Delete]}" delete-char
