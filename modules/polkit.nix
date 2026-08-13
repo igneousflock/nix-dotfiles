1dcba0bb4a943149c8decc4ade88f0ee9a6d6be9
Alex van de Sandt <me@alexvds.com>
2026-08-12 22:23:41 -0500

    Enable passwordless sudo for igneous

--- modules/polkit.nix
+++ modules/polkit.nix
@@ -1,6 +1,21 @@
 {
   den.aspects.polkit = {
     nixos = {
+      security.sudo = {
+        enable = true;
+        extraRules = [
+          {
+            commands = [
+              {
+                command = "ALL";
+                options = [ "NOPASSWD" ];
+              }
+            ];
+            groups = [ "wheel" ];
+          }
+        ];
+      };
+
       security.polkit = {
         enable = true;
         adminIdentities = [ "unix-group:wheel" ];
