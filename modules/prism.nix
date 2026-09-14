47dd24223efd4287ac93d6a454c789646031447a
Alex van de Sandt <me@alexvds.com>
2026-09-14 13:06:43 -0500

    Pin prism java version

--- modules/prism.nix
+++ modules/prism.nix
@@ -1,7 +1,23 @@
 {
   den.aspects.prism = {
     homeManager = { pkgs, ... }: {
-      home.packages = [ pkgs.prismlauncher ];
+      home.packages = [ pkgs.jdk25 ];
+
+      programs.prismlauncher = {
+        enable = true;
+        settings = {
+          ApplicationTheme = "dark";
+          AutomaticJavaDownload = false;
+          IconTheme = "pe_light";
+          JavaPath = "${pkgs.jdk25}/bin/java";
+          Language = "en_US";
+          LastHostname = "flock";
+          MaxMemAlloc = 8192;
+          MinMemAlloc = 1024;
+          UseSystemLocale = true;
+          UserAskedAboutAutomaticJavaDownload = true;
+        };
+      };
     };
   };
 }
